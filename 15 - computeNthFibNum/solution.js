18c74a434322e677fdf9e7881dbe67fc5bb66783
Dennis Dang <dangggdennis@gmail.com>
2018-04-02 22:17:04 -0700

    cake 15 done

--- 15 - computeNthFibNum/solution.js
+++ 15 - computeNthFibNum/solution.js
@@ -8,3 +8,53 @@
  * fib(3);  // => 2
  * fib(4);  // => 3
  */
+
+// Solution 1 - Recursive and memoization
+
+// class Fib {
+//     constructor() {
+//         this.memo = {};
+//     }
+
+//     findFib(num) {
+//         if (num == 0 || num == 1) {
+//             return num;
+//         }
+
+//         if (this.memo.hasOwnProperty(num)) {
+//             return this.memo[num];
+//         }
+
+//         const result = this.findFib(num - 1) + this.findFib(num - 2);
+
+//         this.memo[num] = result;
+
+//         return result;
+//     }
+// }
+
+// const fib = new Fib()
+// fib.findFib(5)
+
+function fib(num) {
+    if (num < 0) {
+        throw new Error(`Can't have negative fibs`);
+    }
+    if (num == 0 || num == 1) {
+        return num;
+    }
+
+    let prevPrev = 0;
+    let prev = 1;
+    let current = 0;
+
+    for (let i = 0; i < num; i++) {
+        current = prevPrev + prev;
+        prevPrev = prev;
+        prev = current;
+    }
+
+    return current;
+}
+
+fib(5);
---
+++ 16 - maxDuffelBagValue/solution.js
@@ -0,0 +1,14 @@
+/*
+Write a function maxDuffelBagValue() that takes an 
+array of cake type objects and a weight capacity, and 
+returns the maximum monetary value the duffel bag can hold.
+*/
+
+function maxDuffelBagValue(types, capacity) {}
+
+var cakeTypes = [{ weight: 7, value: 160 }, { weight: 3, value: 90 }, { weight: 2, value: 15 }];
+
+var capacity = 20;
+
+maxDuffelBagValue(cakeTypes, capacity);
+// returns 555 (6 of the middle type of cake and 1 of the last type of cake)
