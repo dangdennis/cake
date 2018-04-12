31b1a20c1b9ddc68ee873d42a76c99041c5c77cd
Dang <Dennis.Dang@experian.com>
2018-04-12 14:23:17 -0700

    cake 25 & 26 complete, learning typescript starting cake 27

---
+++ 27 - reverseWords/solution.js
@@ -0,0 +1,27 @@
+/*
+Write a function reverseWords() that takes a message as an array of characters
+and reverses the order of the words in-place.
+*/
+function reverseWords(message) {
+    return 5;
+}
+var message = [
+    "c",
+    "a",
+    "k",
+    "e",
+    " ",
+    "p",
+    "o",
+    "u",
+    "n",
+    "d",
+    " ",
+    "s",
+    "t",
+    "e",
+    "a",
+    "l"
+];
+reverseWords(message);
+console.log(message.join(""));
---
+++ 27 - reverseWords/solution.ts
@@ -0,0 +1,35 @@
+/*
+Write a function reverseWords() that takes a message as an array of characters 
+and reverses the order of the words in-place.
+*/
+
+function reverseWords(message: Array<string>)  {
+    return 5
+}
+
+interface Message {
+    
+}
+
+const message = [
+    "c",
+    "a",
+    "k",
+    "e",
+    " ",
+    "p",
+    "o",
+    "u",
+    "n",
+    "d",
+    " ",
+    "s",
+    "t",
+    "e",
+    "a",
+    "l"
+];
+
+reverseWords(message);
+
+console.log(message.join(""));
