278b894fe8a9b0f8594c42d7877f34b5b16bf482
Dennis Dang <dangggdennis@gmail.com>
2018-04-26 08:46:08 -0700

    Cake 28 complete

---
+++ 28 - parenthesisMatching/solution.js
@@ -0,0 +1,35 @@
+/**
+ * Write a function that, given a sentence like the one above,
+ * along with the position of an opening parenthesis,
+ * finds the corresponding closing parenthesis.
+ */
+
+const testString =
+    "Sometimes (when I nest them (my parentheticals) too " +
+    "much (like this (and this))) they get confusing.";
+
+function findMatchingParenthesis(string, initialParensPosition = 0) {
+    if (!string) {
+        throw new Error('Please input a valid string with at least one "("');
+    }
+
+    let openParensCount = 0;
+
+    for (let position = initialParensPosition + 1; position < string.length; position++) {
+        let char = string[position];
+
+        if (char === "(") {
+            openParensCount++;
+        } else if (char === ")") {
+            if (openParensCount === 0) {
+                return position;
+            } else {
+                openParensCount--;
+            }
+        }
+    }
+
+    throw new Error("No closing parenthesis!");
+}
+
+console.log(findMatchingParenthesis(testString, 10));
