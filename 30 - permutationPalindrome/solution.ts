17104933810802964f6ffc10072ad6c0450991cb
Dennis Dang <dangggdennis@gmail.com>
2018-04-28 11:23:40 -0700

    Cake 30 complete

--- 30 - permutationPalindrome/solution.ts
+++ 30 - permutationPalindrome/solution.ts
@@ -5,18 +5,23 @@
  * I even know interviewers who just want to hear you say "object",
  * and once they hear that they'll say, "Great, let's move on."
  */
-function isAnyPermutationPalindrome(sentence) {
-    var set = new Set();
-    for (var i = 0; i < sentence.length; i++) {
+
+function isAnyPermutationPalindrome(sentence: string): boolean {
+    const set = new Set();
+
+    for (let i = 0; i < sentence.length; i++) {
         if (!set.has(sentence[i])) {
             set.add(sentence[i]);
         } else {
-            set["delete"](sentence[i]);
+            set.delete(sentence[i]);
         }
     }
+
     if (set.size <= 1) return true;
+
     return false;
 }
+
 console.log(isAnyPermutationPalindrome("civic"));
 console.log(isAnyPermutationPalindrome("ivicc"));
 console.log(isAnyPermutationPalindrome("civil"));
