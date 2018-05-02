488d3172cd9d0e93f10aea99d53d61f68f35b1f5
Dennis Dang <dangggdennis@gmail.com>
2018-05-01 18:59:07 -0700

    cake 33 complete

--- 33 - whichAppearsTwice/solution.js
+++ 33 - whichAppearsTwice/solution.js
@@ -4,4 +4,21 @@ Every number in the range 1..n1..n appears once except
 for one number that appears twice.
 
 Write a function for finding the number that appears twice
+
 */
+
+/**
+ * Wthell math: triangular series
+ * Each number in a triangular series has a pairing that will equal n+1
+ * Subtract the perfect sum and the imperfect sum will yield that non-paired digit
+ */
+
+function findDouble(digits) {
+    const n1 = digits[digits.length - 1];
+    const sumPerfectTriSeries = (n1 ** 2 + n1) / 2;
+    const sumImperfectTriSeries = digits.reduce((sum, next) => sum + next, 0);
+    console.log(sumImperfectTriSeries, sumPerfectTriSeries);
+    return sumPerfectTriSeries - sumImperfectTriSeries;
+}
+
+console.log(findDouble([1, 2, 3, 4, 5, 6, 8]));
