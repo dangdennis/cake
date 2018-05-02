d716737ebd0853c6ca779299479e9edd2d8a1189
Dennis Dang <dangggdennis@gmail.com>
2018-05-01 19:04:59 -0700

    Cake 34 WIP

---
+++ 34 - wordCloudData/solution.js
@@ -0,0 +1,9 @@
+/* 
+Write code that takes a long string and builds its 
+word cloud data in a map, where the keys are words 
+and the values are the number of times the words occurred.
+
+Think about capitalized words
+*/
+
+function makeWordCloud(str) {}
