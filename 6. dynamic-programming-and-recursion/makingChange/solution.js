4e4e57ffce6374752b9d7fb24cd1a1e929799bdf
Dennis Dang <dangggdennis@gmail.com>
2018-07-07 12:37:04 -0700

    add recursive solution to make change

--- 6. dynamic-programming-and-recursion/makingChange/solution.js
+++ 6. dynamic-programming-and-recursion/makingChange/solution.js
@@ -1,5 +1,60 @@
-function changePossibilitiesBottomUp(amount, denominations) {
+// recursion
 
+class Change {
+    constructor() {
+        this.memo = {};
+    }
+
+    changePossibilitiesTopDown(amountLeft, denominations, currentIndex = 0) {
+        // Check our memo and short-circuit if we've already solved this one
+        const memoKey = [amountLeft, currentIndex].join(", ");
+        if (this.memo.hasOwnProperty(memoKey)) {
+            console.log("grabbing memo [" + memoKey + "]");
+            return this.memo[memoKey];
+        }
+
+        // Base cases:
+        // We hit the amount spot on. yes!
+        if (amountLeft === 0) return 1;
+
+        // We overshot the amount left (used too many coins)
+        if (amountLeft < 0) return 0;
+
+        // We're out of denominations
+        if (currentIndex === denominations.length) return 0;
+
+        console.log(
+            "checking ways to make " +
+                amountLeft +
+                " with [" +
+                denominations.slice(currentIndex).join(", ") +
+                "]",
+        );
+
+        // Choose a current coin
+        const currentCoin = denominations[currentIndex];
+
+        // See how many possibilities we can get
+        // for each number of times to use currentCoin
+        let numPossibilities = 0;
+        while (amountLeft >= 0) {
+            numPossibilities += this.changePossibilitiesTopDown(
+                amountLeft,
+                denominations,
+                currentIndex + 1,
+            );
+            amountLeft -= currentCoin;
+        }
+
+        // Save the answer in our memo so we don't compute it again
+        this.memo[memoKey] = numPossibilities;
+        return numPossibilities;
+    }
+}
+
+
+// Non recursion, bottoms up
+function changePossibilitiesBottomUp(amount, denominations) {
     // intialize an array of zeros with indices up to amount
     var waysOfDoingNcents = [];
     for (var i = 0; i <= amount; i++) {
@@ -17,4 +72,4 @@ function changePossibilitiesBottomUp(amount, denominations) {
     return waysOfDoingNcents[amount];
 }
 
-changePossibilitiesBottomUp(5, [1,3,5])
+changePossibilitiesBottomUp(5, [1, 3, 5]);
